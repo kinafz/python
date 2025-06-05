196cf3afb41f67a63780b37f0a71a99adcc6eb9c
Aurélien FILEZ <aurelien.filez@gmail.com>
2025-06-05 13:42:04 +0200

    :art: Add RotatingFileHandler

--- logger.py
+++ logger.py
@@ -1,9 +1,13 @@
 import logging
+from logging.handlers import RotatingFileHandler
 from config import logPath
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
-    filename=logPath)
+    handlers=[
+        RotatingFileHandler(logPath, maxBytes=2000, backupCount=10),
+        logging.StreamHandler()
+    ])
 
 logger = logging.getLogger()
